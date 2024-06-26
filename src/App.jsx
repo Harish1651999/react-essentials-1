17452a61019d435a47405f186c2313092528b8f8
Harish1651999 <hk1651999@gmail.com>
2024-06-26 09:47:53 +0530

    manage state and render content conditionally.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
   function handleClick(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a button</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -15,10 +33,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...EXAMPLES[0]} />
+            <CoreConcept {...EXAMPLES[1]} />
+            <CoreConcept {...EXAMPLES[2]} />
+            <CoreConcept {...EXAMPLES[3]} />
           </ul>
         </section>
         <section id="examples">
@@ -31,7 +49,7 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          Dynamic content
+          {tabContent}
         </section>
       </main>
     </div>
